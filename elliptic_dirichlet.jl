dacdd14cc8a2c04f81bdf987065ec12c76ff7bef
Ayushk4 <ayushk4@gmail.com>
2019-10-03 22:08:12 +0530

    Fix the weak solution training bugz

--- elliptic_dirichlet.jl
+++ elliptic_dirichlet.jl
@@ -6,8 +6,8 @@ using Random
 d = 5                   # Number of dimensions
 Kφ = 1                  # Num iters for adversarial network
 Kᵤ = 2                  # Num iters for solutions
-τη = Float32(0.04)      # Learning rate for adversary network
-τθ = Float32(0.015)     # Learning rate for primal network
+τη = 0.04               # Learning rate for adversary network
+τθ = 0.015              # Learning rate for primal network
 Nr = 4000 * d           # No. of sampled pts in region
 Nb = 40 * d * d         # No. of sampled point on boundary
 α = 10000               # Weight paramter on boundary
@@ -24,20 +24,21 @@ f(x, _a, _ρ0, _ρ1, _cos) = 4 * _ρ1 * _a * sin(_ρ0) - 4 * _ρ0 * _cos -
 f(x, _a, _ρ0, _ρ1) = f(x, _a, _ρ0, _ρ1, cos(_ρ0))
 f(x) = f(x, a0(x), ρ0(x), ρ1(x))
 
-grad_uθ(x) = Tracker.gradient((x) -> uθ(x)[1], x)[1]
+grad_uθ(x) = Tracker.gradient((x) -> sum(uθ(x)), x; nest=true)[1]
 # ∇_dot_a∇u(x) = Tracker.jacobian(x-> a0(x) .* ∇u(x), x)
-grad_φη(x) = Tracker.gradient((x) -> φη(x)[1], x)[1]
+grad_φη(x) = Tracker.gradient((x) -> sum(φη(x)), x; nest=true)[1]
 
+# grad_
 I(x, _∇uθ) = - (_∇uθ' * grad_φη(x))[1] * a0(x) + (φη(x)[1]) * (sum(_∇uθ .^ 2)/2 - f(x))
 I(x) = I(x, grad_uθ(x))
 
 # Primal networks - weak solution to PDE
 uθ = Flux.Chain(Dense(d, hlsθ, tanh),
-                Dense(hlsθ, hlsθ, tanh),
-                Dense(hlsθ, hlsθ, softplus),
-                Dense(hlsθ, hlsθ, tanh),
-                Dense(hlsθ, hlsθ, softplus),
-                Dense(hlsθ, hlsθ, tanh),
+                # Dense(hlsθ, hlsθ, tanh),
+                # Dense(hlsθ, hlsθ, softplus),
+                # Dense(hlsθ, hlsθ, tanh),
+                # Dense(hlsθ, hlsθ, softplus),
+                # Dense(hlsθ, hlsθ, tanh),
                 Dense(hlsθ, 1)
     )
 
@@ -53,22 +54,28 @@ uθ = Flux.Chain(Dense(d, hlsθ, tanh),
                 Dense(hlsη, 1)
     )
 
-# loss_int(x) =
-loss_bndry(x) = sum((uθ(x) .- g(x)) .^ 2))
+u_true(x) = sin.((π .* view(x, 1, :, :) .^ 2 + view(x, 2, :, :) .^ 2) ./ 2)
+g(x) = u_true(x)
+
+# loss_int.(x) =
+loss_bndry(x) = sum((uθ(x) .- g(x)') .^ 2)
 loss(xr, xb) = loss_int(xr) + α * loss_bndry(xb)
 loss(xr) = loss_int(xr)
 
-optθ = Flux.Optimise.ADAGrad(η = τθ)
-optη = Flux.Optimise.ADAGrad(η = τη)
+# optθ = Flux.Optimise.ADAGrad(τθ)
+# optη = Flux.Optimise.ADAGrad(τη)
+
+optθ = Flux.Optimise.Descent(τθ)
+optη = Flux.Optimise.Descent(τη)
 
 psθ = params(uθ)
 psη = params(φη)
 
-boundary_iter = collect(partition(1:Nb, Int(Nb/(2d))))
+# boundary_iter = collect(partition(1:Nb, ))
 
 function train_step()
     # sample points
-    xr = 2 .* rand(Float32, d, Nr) .- 1 # Sampling in region
+    xr = [2 .* rand(Float32, d, 1) .- 1 for i in 1:Nb] # Sampling in region
     xb = 2 .* rand(Float32, d, Nb) .- 1
     for i in 1:Nb
         (j=rand(1:10)) <= d ? xb[j, i] = 1 : xb[j - 5, i] = -1
@@ -76,9 +83,9 @@ function train_step()
 
     for i in 1:Kᵤ
         # update weak solution network parameter
-        l = loss(xr, xb)
-        gradsθ = Tracker.gradient(() -> l, psθ)
-        Tracker.update!(optθ, psθ, gradsθ)
+        # l = loss(xr, xb)
+        gradsθ = Flux.Tracker.gradient(() -> loss(xr, xb), psθ)
+        Flux.Tracker.update!(optθ, psθ, gradsθ)
     end
 
     for i in 1:Kφ
